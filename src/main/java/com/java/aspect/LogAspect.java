9b35551e4e1e69ba28c1ef3c5eed85f13bbf5dc2
WinriseF <3098274040@qq.com>
2025-10-26 21:20:56 +0800

    优化10.26通过 AOP 为StudentService的方法添加操作日志注解

--- src/main/java/com/java/aspect/LogAspect.java
+++ src/main/java/com/java/aspect/LogAspect.java
@@ -1,14 +1,25 @@
 package com.java.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
 import java.util.Arrays;
 import java.util.Date;
 
+@Aspect
+@Component
 public class LogAspect {
+
+    @Before("execution(* com.java.service.StudentService.*(..))")
     public void logBefore(JoinPoint joinPoint) {
         System.out.println("方法 " + joinPoint.getSignature().getName() + " 开始执行，参数：" +
                 Arrays.toString(joinPoint.getArgs()) + "，时间：" + new Date());
     }
+
+    @After("execution(* com.java.service.StudentService.*(..))")
     public void logAfter(JoinPoint joinPoint) {
         System.out.println("方法 " + joinPoint.getSignature().getName() + " 执行结束，时间：" + new Date());
     }
