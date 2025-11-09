524e434f029eac65ee7e9e7bf76bfe1c9eb3ac60
WinriseF <3098274040@qq.com>
2025-11-09 21:35:12 +0800

    11.9注解方法配置日志切面

--- src/main/java/com/java/aspect/PermissionAspect.java
+++ src/main/java/com/java/aspect/PermissionAspect.java
@@ -7,12 +7,12 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-//@Aspect
-//@Component
-//@Order(1)
+@Aspect
+@Component
+@Order(1)
 public class PermissionAspect {
 
-//    @Before("execution(* com.java.service.StudentService.*(..))")
+    @Before("execution(* com.java.service.StudentService.*(..))")
     public void checkPermission(JoinPoint joinPoint) {
         System.out.println("权限检查通过");
     }
